3acf0d3576fbf4d89f72e333fbc5ac0af11bcf93
Andini <arunafallian@gmail.com>
2024-02-03 09:32:58 +0800

    first commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,10 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Pages::index');
+$routes->get('/pages', 'Pages::index');
+$routes->get('/pages/about', 'Pages::about');
---
+++ app/Views/pages/about.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>Halaman About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa assumenda in enim, expedita, veritatis delectus quae debitis aliquam nulla perspiciatis, consequatur sunt aliquid suscipit iusto nisi ducimus quod molestias earum?</p>
+        </div>
+    </div>
+</div>
